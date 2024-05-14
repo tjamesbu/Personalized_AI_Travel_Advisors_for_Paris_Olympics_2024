fa4d03731a575d58fd112e04cca5042dc3b8817b
Gaurav Wani <wanigaurav11@gmail.com>
2024-05-14 17:22:51 +0000

    Upload files to 'src/tasks/task2'

--- src/tasks/task2/GauravWani_prompt.py
+++ src/tasks/task2/GauravWani_prompt.py
@@ -2,3 +2,10 @@
 iteneary_for_trip="""
         List down 5 cheaper accomodation in Paris which provide brakfast
       """
+
+iteneary_for_trip="""
+        Provide best route to reach Bercy Arena , France, Paris from Hôtel Caumartin Opéra and how should i travel?
+      """
+iteneary_for_trip="""
+        Extract 10 affordable and safe accommodation options near public transportation, so I can find a comfortable place to stay that fits my budget and allows me to easily explore the city in Paris area
+      """
