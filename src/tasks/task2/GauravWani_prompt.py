20eb43d19695c80bb65feeb6b23da705cbf0b00c
Gaurav Wani <wanigaurav11@gmail.com>
2024-05-14 16:27:20 +0000

    Upload files to 'src/tasks/task2'

--- src/tasks/task2/GauravWani_prompt.py
+++ src/tasks/task2/GauravWani_prompt.py
@@ -1,21 +1,4 @@
-#Personlaised trip prompt
 
 iteneary_for_trip="""
-    Create Itineraries and Local Hotspot Recommendations 
-    around Handball, France for two days, 
-    this should include restaurants as well
-    
-    List of Topics:
-    - Hidden gem attractions
-    - Authentic local restaurants
-    - Charming neighborhoods
-    - Lesser-known cultural sites
-    - Special events or festivals
-    - Immersing in local culture
-
-    - Insider tips for navigating Lyon like a local
-    - Suggestions for transportation options within the city
-    - Recommendations for interacting with locals and understanding the cultural norms
-    - Safety tips for exploring off-the-beaten-path locations
-    - Ideas for capturing memorable moments through photography or journaling
-    """
+        List down 5 cheaper accomodation in Paris which provide brakfast
+      """
