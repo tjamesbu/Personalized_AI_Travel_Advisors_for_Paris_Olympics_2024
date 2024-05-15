fbd5df636cdacec6fb334728f7abd7b3b5df9a82
Gaurav Wani <wanigaurav11@gmail.com>
2024-05-15 15:08:35 +0000

    Upload files to 'src/tasks/task2'

--- src/tasks/task2/GauravWani_prompt.py
+++ src/tasks/task2/GauravWani_prompt.py
@@ -9,3 +9,7 @@ iteneary_for_trip="""
 iteneary_for_trip="""
         Extract 10 affordable and safe accommodation options near public transportation, so I can find a comfortable place to stay that fits my budget and allows me to easily explore the city in Paris area
       """
+
+iteneary_for_trip="""
+        Hi there! I will be travelling to Paris for Olympics 2024, can you please provide list of unexplored places which are not overcrowded and having good public transport
+      """
